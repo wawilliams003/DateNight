17e3c69336301482d990127628eb729ae726b2b5
Wayne <wawilliams003@gmail.com>
2022-12-15 01:31:25 -0500

    Major ui changes

--- DateNight/HomeViewController.swift
+++ DateNight/HomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Koloda
 import LGButton
+import iCarousel
 
 class HomeViewController: UIViewController {
 
@@ -15,8 +16,29 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var kView: KolodaView!
     
     
+    //MARK: Properties
     
-    let cards = ["dr", "thor", "ironman", "spider", "avenger"]
+    let myCarousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .rotary
+        view.scrollSpeed = 0.5
+       // view.contentOffset = CGSize(width: 50, height: 50)
+        //view.numberOfVisibleItems = 5
+        return view
+    }()
+    
+    let fontCarousel: iCarousel = {
+        let view = iCarousel()
+        view.type = .rotary
+        view.scrollSpeed = 0.5
+        
+        return view
+    }()
+    
+    //let cards = ["dr", "thor", "ironman", "spider", "avenger"]
+    
+    let cards = ["Do you believe in love?", "Do you believe in love at first sight?", "Have you ever been in love?", "What is your definition of love?", "Do you love yourself?"]
+    
     
     
     
@@ -24,6 +46,16 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         
         setupKView()
+        
+        view.addSubview(myCarousel)
+        view.addSubview(fontCarousel)
+        fontCarousel.isHidden = true
+        myCarousel.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 200)
+        fontCarousel.frame = CGRect(x: 0, y: 380, width: view.frame.size.width, height: 100)
+        myCarousel.dataSource = self
+        myCarousel.delegate = self
+        fontCarousel.dataSource = self
+        fontCarousel.delegate = self
         
         // Do any additional setup after loading the view.
     }
@@ -35,6 +67,14 @@ class HomeViewController: UIViewController {
         kView.clipsToBounds = true
         
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        UIView.transition(with: view, duration: 0.7, options: .transitionCrossDissolve,  animations: { [weak self] in
+            self?.fontCarousel.isHidden = true
+        })
+    }
+    
 
    //MARK: Buttons
    
@@ -58,7 +98,8 @@ class HomeViewController: UIViewController {
     }
     
     @IBAction func likeCard()  {
-        
+        fontCarousel.isHidden = false
+        UIView.transition(with: fontCarousel, duration: 0.5, options: .transitionCrossDissolve,  animations: nil)
         
     }
     
@@ -68,6 +109,70 @@ class HomeViewController: UIViewController {
     }
     
 }
+
+//MARK: MyCarousel Delegate
+
+extension HomeViewController: iCarouselDataSource {
+    
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        if carousel == myCarousel {
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width/4, height: 150))
+            view.backgroundColor = .purple
+            view.layer.cornerRadius = 10
+            view.layer.borderColor = UIColor.white.cgColor
+            view.layer.borderWidth = 1
+            view.clipsToBounds = true
+            return view
+        }
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width/4, height: 50))
+        let label = UILabel(frame: view.frame)
+        view.addSubview(label)
+        label.text = "Aa"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 20)
+        view.backgroundColor = .brown
+        view.layer.cornerRadius = 10
+        view.layer.borderColor = UIColor.white.cgColor
+        view.layer.borderWidth = 1
+        view.clipsToBounds = true
+        return view
+        
+    }
+    
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return 8
+    }
+    
+    
+    
+    
+}
+
+
+extension HomeViewController: iCarouselDelegate {
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+        
+        switch option {
+        case .spacing: return value * 1.75
+            
+        case .visibleItems: return 3
+            
+        //case .offsetMultiplier: return 2
+        default: return value
+        }
+        
+    }
+    
+//    func carouselItemWidth(_ carousel: iCarousel) -> CGFloat {
+//        return 150
+//    }
+    
+}
+
 
 
 //MARK: Delegate
@@ -91,6 +196,23 @@ extension HomeViewController: KolodaViewDataSource {
     
     func koloda(_ koloda: Koloda.KolodaView, viewForCardAt index: Int) -> UIView {
         let kView = UIImageView(image: UIImage(named: cards[index]))
+        let coverView = UIView(frame: CGRect(x: 0, y: 0, width: koloda.frame.width, height: 150))
+        coverView.backgroundColor = .black
+        coverView.layer.opacity = 0.050
+        kView.addSubview(coverView)
+        kView.image = UIImage(named: "border")
+        kView.backgroundColor = .purple
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.clipsToBounds = true
+        label.shadowColor = UIColor.darkGray
+        label.layer.shadowOpacity = 0.5
+        kView.addSubview(label)
+        //label.adjustsFontSizeToFitWidth = true
+        label.frame = CGRect(x: 20, y: 35, width: koloda.frame.width-20, height: 200)
+        label.text = cards[index]
         kView.layer.cornerRadius = 20
         kView.clipsToBounds = true
         
